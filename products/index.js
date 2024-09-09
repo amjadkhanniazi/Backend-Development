dc2d3b87a48a0692cf8d95df31006c342ba035f6
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2024-09-09 01:19:29 -0700

    auth added to Products-->index.js

--- products/index.js
+++ products/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import products from './models/products.js';
+import user from './models/user.js';
+import 'dotenv/config';
+import authenticateToken from './middlewares/auth.js';
 
 const app = express();
 
@@ -11,33 +14,63 @@ app.use(cors());
 
 connectDB();
 
-app.get('/products', async (req,res)=>{
-    const product= await products.find();
+app.get('/products', authenticateToken, async (req, res) => {
+    const product = await products.find();
     res.json(
-        {product}
+        { product }
     );
 })
 
-app.post('/products/new',async (req,res) => {
-    const newProduct=req.body;
+app.post('/products/new', authenticateToken, async (req, res) => {
+    const newProduct = req.body;
     await products.create(newProduct);
     res.json({
         message: 'Product added'
     });
 })
 
-app.get('/products/:id', async (req,res) => {
-    const product=await products.findById(req.params.id);
+app.get('/products/:id', authenticateToken, async (req, res) => {
+    const product = await products.findById(req.params.id);
     res.json({
         product
     });
 })
 
-app.delete('/products/:id', async (req,res) =>{
+app.delete('/products/:id', authenticateToken, async (req, res) => {
     await products.findByIdAndDelete(req.params.id);
     res.json({
         message: 'Product deleted'
     });
 })
 
-app.listen(5011,()=>console.log('http://localdb:5011'));
+// register new user
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try{
+        const newUser= new user({username,password});
+        await newUser.save();
+        res.status(200).json({message:'User Created'});
+    } catch(error){
+        res.status(400).json({error: error.message});
+    }
+});
+
+// login
+app.post('/login', async (req,res)=>{
+    const {username, password}=req.body;
+    try{
+        const newUser= await  user.findOne({username});
+        if (!newUser) res.status(400).json({error: 'Invalid Credentials'});
+
+        const isMatch= await newUser.comparePassword(password);
+        if (!isMatch) res.status(400).json({error: 'Invalid Credentials'});
+
+        const token = newUser.getToken();
+        res.json({token})
+    }
+    catch (error) {
+        res.status(500).json({error: error.message});
+    }
+})
+
+app.listen(5011);
