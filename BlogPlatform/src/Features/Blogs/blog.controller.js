65e58561240bf98de6c61cb4f694c3a48f6e9a6b
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2024-10-10 13:00:55 +0500

    "Added delete, get, get all, and update blog functionality to blog controller and routes"

--- BlogPlatform/src/Features/Blogs/blog.controller.js
+++ BlogPlatform/src/Features/Blogs/blog.controller.js
@@ -1,6 +1,7 @@
 const Blog = require('./blog.model');
 const User = require('../Users/user.model');
 
+//create a blog
 const createBlog = async (req, res) => {
     try {
         const { title, content } = req.body;
@@ -13,5 +14,60 @@ const createBlog = async (req, res) => {
     }
 }
 
+//delete a blog
+const deleteBlog = async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+        if (blog.user.toString() === req.user.id) {
+            await blog.deleteOne();
+            res.status(200).json({ message: "Blog Deleted" });
+        } else {
+            res.status(401).json({ message: "Unauthorized" });
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
 
-module.exports = { createBlog };
+//get a blog
+const getBlog = async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id).populate({
+            path: 'user',
+            select: '-password -email' // exclude password email in the populated user field
+        });
+        res.status(200).json(blog);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+//get all blogs
+const getBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find().populate({
+            path: 'user',
+            select: '-password -email' // exclude password email in the populated user field
+        });
+        res.status(200).json(blogs);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+//update a blog
+const updateBlog = async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        await Blog.findByIdAndUpdate(req.params.id, { title, content }, { new: true });
+        res.status(200).json({ message: "Blog Updated" });
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+
+module.exports = { 
+    createBlog, deleteBlog, getBlog, getBlogs, updateBlog
+};
--- BlogPlatform/src/Features/Blogs/blog.routes.js
+++ BlogPlatform/src/Features/Blogs/blog.routes.js
@@ -5,6 +5,11 @@ const router = require('express').Router();
 
 
 router.post('/create', authenticateToken, Blog.createBlog);
+router.delete('/delete/:id', authenticateToken, Blog.deleteBlog);
+router.get('/get/:id', Blog.getBlog);
+router.get('/get', Blog.getBlogs);
+router.put('/update/:id', authenticateToken, Blog.updateBlog);
+
 
 
 
